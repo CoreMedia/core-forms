2df2c04f24ea591acae88d78fcae5a88161e60bf
Philipp Hofmann <philipp.hofmann@coremedia.com>
2022-08-05 11:52:57 +0200

    removed warnings

--- apps/studio-client/shared/js/form-editor/src/init.ts
+++ apps/studio-client/shared/js/form-editor/src/init.ts
@@ -11,10 +11,7 @@ contentTypeLocalizationRegistry.addLocalization("FormEditor", {
       displayName: FormEditorDocTypes_properties.FormEditor_adminMails_displayName,
       emptyText: FormEditorDocTypes_properties.FormEditor_adminMails_emptyText,
     },
-    formAction_group: { displayName: FormEditorDocTypes_properties.FormEditor_formAction_group_displayName },
     formData: { displayName: FormEditorDocTypes_properties.FormEditor_formData_displayName },
     spamProtectionEnabled: { displayName: FormEditorDocTypes_properties.FormEditor_spamProtectionEnabled_displayName },
-    spamProtectionEnabled_true: { displayName: FormEditorDocTypes_properties.FormEditor_spamProtectionEnabled_true_displayName },
-    spamProtectionEnabled_group: { displayName: FormEditorDocTypes_properties.FormEditor_spamProtectionEnabled_group_displayName },
   },
 });
